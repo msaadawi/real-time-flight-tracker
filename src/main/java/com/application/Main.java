a6eb405f766c02e5f2f997f8e15af522593e4c13
saadaoui majd <saadaouimajd307pgmail.com>
2019-01-31 11:44:26 +0100

    fixed thread Exception

--- src/main/java/com/application/Main.java
+++ src/main/java/com/application/Main.java
@@ -81,7 +81,7 @@ public class Main extends Application {
                     }
                     catch (Exception e)
                     {
-                        e.printStackTrace();
+                        continue;
                     }
                 }
             }
